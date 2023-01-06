e90964e24f1e9517ef2f241f24438000a43b42d0
dbadoy4874 <dbadoy4874@gmail.com>
2023-01-07 02:50:36 +0900

    node: add ABI, member control methods

--- node/node.go
+++ node/node.go
@@ -1,8 +1,11 @@
 package node
 
 import (
+	"context"
+	"fmt"
 	"net/rpc"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/eventories/go-eventories/core"
 	"github.com/eventories/go-eventories/core/table"
 	"github.com/eventories/go-eventories/p2p"
@@ -14,4 +17,34 @@ type Node struct {
 	tab  *table.Table
 	srv  *p2p.Server
 	http *rpc.Server
+}
+
+func (n *Node) ABI(name string) (*abi.ABI, error) {
+	abi := n.tab.ABI(name)
+	if abi == nil {
+		return nil, fmt.Errorf("non exist abi %s", name)
+	}
+	return abi, nil
+}
+
+func (n *Node) Members() []string {
+	return n.srv.Cluster()
+}
+
+func (n *Node) AddMember(rawaddr string) error {
+	req := &p2p.AddMemberReq{Member: rawaddr}
+	b, err := p2p.EncodeRequest(req)
+	if err != nil {
+		return err
+	}
+	return n.srv.Commit(context.Background(), nil, b)
+}
+
+func (n *Node) DelMember(rawaddr string) error {
+	req := &p2p.DelMemberReq{Member: rawaddr}
+	b, err := p2p.EncodeRequest(req)
+	if err != nil {
+		return err
+	}
+	return n.srv.Commit(context.Background(), nil, b)
 }
