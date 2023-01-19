ce2bff2608b810cfb7a28a5fd6996b60bb3db721
dbadoy4874 <dbadoy4874@gmail.com>
2023-01-19 14:35:42 +0900

    core: add `Checkpoint`

--- core/checkpoint.go
+++ core/checkpoint.go
@@ -7,37 +7,45 @@ import (
 	"sync/atomic"
 )
 
-type checkpoint struct {
-	path  string
-	block uint64
+const defaultPath = "/checkpoint"
+
+type Checkpoint struct {
+	kind string
+	n    uint64
 }
 
-func newCheckpoint(path string) *checkpoint {
-	_, err := ioutil.ReadDir(path)
+func NewCheckpoint(kind string) *Checkpoint {
+	_, err := ioutil.ReadDir(defaultPath)
 	if err != nil {
-		return &checkpoint{path, 0}
+		return &Checkpoint{defaultPath, 0}
 	}
 
-	bn, err := ioutil.ReadFile(path + "checkpoint")
+	n, err := ioutil.ReadFile(defaultPath + "/" + kind)
 	if err != nil {
-		return &checkpoint{path, 0}
+		return &Checkpoint{defaultPath, 0}
 	}
 
-	return &checkpoint{path, binary.BigEndian.Uint64(bn)}
+	return &Checkpoint{defaultPath, binary.BigEndian.Uint64(n)}
 }
 
-func (c *checkpoint) blockNumber() uint64 {
-	return atomic.LoadUint64(&c.block)
+func (c *Checkpoint) Checkpoint() uint64 {
+	return atomic.LoadUint64(&c.n)
 }
 
-func (c *checkpoint) setBlockNumber(bn uint64) error {
+func (c *Checkpoint) SetCheckpoint(n uint64) error {
 	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, bn)
+	binary.BigEndian.PutUint64(b, n)
 
-	if err := ioutil.WriteFile(c.path+"checkpoint", b, fs.FileMode(1)); err != nil {
+	if err := ioutil.WriteFile(defaultPath+"/"+c.kind, b, fs.FileMode(1)); err != nil {
 		return err
 	}
 
-	atomic.StoreUint64(&c.block, bn)
+	atomic.StoreUint64(&c.n, n)
+	return nil
+}
+
+func (c *Checkpoint) Increase() error {
+	atomic.AddUint64(&c.n, 1)
+	// write files
 	return nil
 }
--- core/fetcher.go
+++ core/fetcher.go
@@ -8,14 +8,21 @@ import (
 type Fetcher struct {
 	eth *interaction.Interactor
 
-	cp *checkpoint
+	cp *Checkpoint
 	db database.Database
 }
 
+func NewFetcher(interact *interaction.Interactor, db database.Database) (*Fetcher, error) {
+	return &Fetcher{
+		eth: interact,
+		cp:  NewCheckpoint("blockNumber"),
+	}, nil
+}
+
 func (f *Fetcher) BlockNumber() uint64 {
-	return f.cp.blockNumber()
+	return f.cp.Checkpoint()
 }
 
 func (f *Fetcher) SetBlockNumber(bn uint64) error {
-	return f.cp.setBlockNumber(bn)
+	return f.cp.SetCheckpoint(bn)
 }
